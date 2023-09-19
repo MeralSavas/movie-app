840b81fbee496e27d8e56dd8b5e90fb250aedfb6
MeralSavas <meralsavas@gmail.com>
2023-09-19 23:38:04 +0100

    detail

--- starter-1/src/auth/firebase.js
+++ starter-1/src/auth/firebase.js
@@ -21,22 +21,11 @@ import { initializeApp } from "firebase/app";
 // TODO: Replace the following with your app's Firebase project configuration at project settings part
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDeJMoAlxTRRiZ6MYX3-Yd_x6nGhyWS_DU",
-//   authDomain: "movie-app-2fb59.firebaseapp.com",
-//   projectId: "movie-app-2fb59",
-//   storageBucket: "movie-app-2fb59.appspot.com",
-//   messagingSenderId: "758382405547",
-//   appId: "1:758382405547:web:82a8461f7c286d230b14ee",
-//   measurementId: "G-083TVDDGMH",
-
-//   // ...
-// };
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
+  projectId: process.env.RE§ACT_APP_projectId,
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
   appId: process.env.REACT_APP_appId,
